d6b5eae414ad38e86cd447c7b83a7ea1074ae1b4
Akmal Oriqov <akmalbekortiqov7976@gmail.com>
2023-01-09 22:34:06 +0500

    switch case 1-7

---
+++ case.js
@@ -0,0 +1,157 @@
+// * Tanlash operatoriga oid masalalar (switch case)
+// *30.12.2022  12:20 
+
+// Case1
+// let n=1
+// switch (n) {
+//     case 1: "dushanba"
+//         break;
+//     case 2: "seshanba"
+//         break;
+//     case 3: "chorshnba"
+//         break;
+//     case 4: "payshanba"
+//         break;
+//     case 5: "juma"
+//         break;
+//     case 6: "shanba"
+//         break;
+//     case 7: "yakshanba"
+
+//     default: "Adashding"
+//         break;
+// }
+
+// case2
+// let n = 4
+// switch (n) {
+//     case 1: "yomon"
+//         break;
+//     case 2: "qoniqarsiz"
+//         break;
+//     case 3: "qoniqarli"
+//         break;
+//     case 4: "yaxshi"
+//         break;
+//     case 5: "alo"
+//         break;
+
+//     default: "xato"
+//         break;
+// }
+
+// case3
+// let n = 3
+// switch (n) {
+//     case 12:
+//     case 1:
+//     case 2: "Qish"
+//         break;
+//     case 3:
+//     case 4:
+//     case 5: "Bahor"
+//         break;
+//     case 6:
+//     case 7:
+//     case 8: "yoz"
+//         break;
+//     case 9:
+//     case 10:
+//     case 11:"kuz"
+
+//     default: "err"
+//         break;
+// }
+
+// case4
+// let n=4
+// switch(n){
+//     case 1:'31'
+//     break;
+//     case 2: '28'
+//         break;
+//     case 3:'31'
+//     break;
+//     case 4:'30'
+//     break;
+//     case 5: "31"
+//         break;
+//     case 6:'30'
+//     break;
+//     case 7:'31'
+//     break;
+//     case 8: "31"
+//         break;
+//     case 9:'30'
+//     break;
+//     case 10:'31'
+//     break;
+//     case 11:"30"
+//     break;
+//     case 12 : '31'
+
+//     default: "err"
+//         break;
+// }
+
+// case5
+// let a = 2
+// let b = 3
+// let c = 2
+// switch (c) {
+//     case 1: a + b
+//         break;
+//     case 2: a - b
+//         break;
+//     case 3: a / b
+//         break;
+//     case 4: a * b
+//     default: 'err'
+//         break;
+// }
+
+// case6
+// let a = 100
+// let b = 2
+// switch (b) {
+//     case 1: a / 10
+//         break;
+//     case 2: a * 1000
+//         break;
+//     case 3: a 
+//         break;
+//     case 4: a /1000
+//         break;
+//     case 5: a / 100
+//         break;
+//     default: "err"
+//         break;
+// }
+
+
+// case7
+// let a = 100
+// let b = 2
+// switch (b) {
+//     case 1: a 
+//         break;
+//     case 2: a * 100000
+//         break;
+//     case 3: a *1000
+//         break;
+//     case 4: a /1000
+//         break;
+//     case 5: a / 10
+//         break;
+//     default: "err"
+//         break;
+// }
+
+// ! case8
+// let D=2
+// let M=0
+
+
+
+
+// case9
